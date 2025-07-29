a11f0e2a01d47c17dbaaa699fd8aaad320a67959
Ayepes2402 <andrea.yepese@upb.edu.co>
2025-07-29 15:53:45 -0500

    intento actividades

---
+++ Experiencia de aprendizaje 2/investigacion.md
@@ -0,0 +1,34 @@
+## Actividad 1
+
+¿Qué es la entrada-salida mapeada a memoria?  
+Es cuando una parte de la memoria del computador no se usa para datos normales, sino que está conectada a los dispositivos de entrada y salida. Entonces al leer o escribir en esas direcciones en realidad estamos leyendo el teclado o dibujando en la pantalla.  
+
+¿Cómo se implementa en la plataforma Hack?  
+La pantalla (@SCREEN) está mapeada desde la dirección 16384 hasta 24575. Cada palabra (16 bits) controla 16 pixeles en blanco/negro.  
+
+El teclado (KBD) está en la dirección 24576. Si se presiona una tecla, esta dirección contiene su código y no hay ninguna tecla presionada, contiene 0.  
+
+Inventa un programa que haga uso de la entrada-salida mapeada a memoria.  
+
+(LOOP)
+    @KBD
+    D=M 
+
+    @109       
+    D=D-A      
+    @CLEAR_LOOP
+    D;JNE  
+
+    @SCREEN
+    M=-1
+    @LOOP
+    0;JMP
+
+(CLEAR_LOOP)
+    @SCREEN
+    M=0
+    @LOOP
+    0;JMP
+
+Investiga el funcionamiento del programa con el simulador.  
+Lo que hace este programa es que si precionas la letra M este enciende los primeros 16 píxeles y si no es M los apaga.
--- Experiencia de aprendizaje 2/reto.asm
+++ Experiencia de aprendizaje 2/reto.asm
@@ -1,11 +1,14 @@
 //Actividad 3
 
-//Paso 1: Me posciono en @24576 y lo guardo con D=M
-//Paso 2: Me posiciono en @100 y despues uso D=D-A y dependiendo del resultado sabe si debe dibujar
-//Paso 4: creo un nuevo loop y me posiciono en @0 o creo una variable para poder poner su valor 0 y asi borre
-//Paso 5: Despues pongo D=M
-//Paso 6: 
+//Intento de actividad en palabras
+//Paso 1: Primero miro la parte del teclado. Si la tecla que está en memoria no es la d, entonces me voy para la parte donde se borra la pantalla.
+//Paso 2: Para empezar a borrar, me voy a la dirección de la pantalla (@16348)
+//Paso 3: Esa dirección la guardo en una variable para ir avanzando de a una.
+//Paso 4: Luego en un ciclo voy poniendo cero en donde esté parado, eso hace que se apague todo ese bloque de puntos.
+//Paso 5: Después de poner cero le sumo uno para pasar a lo que sigue, como si fuera la siguiente parte de la pantalla.
+//Paso 6: Repito lo mismo hasta que llego casi al final, que creo que es antes del teclado, como por el 24500 y algo, y ahí ya paro y vuelvo al inicio.
 
+//Resultado bueno despues del intento
 (LOOP)
 @24576
 D=M
@@ -13,13 +16,163 @@ D=M
 D=D-A       
 @draw
 D;JEQ  
+@CLEAR
+@0
+D=M
+(CLEAR)
+    @SCREEN
+    D=A
+    @addr
+    M=D 
+
+(CLEAR_LOOP)
+    @addr
+    A=M
+    M=0  
+
+    @addr
+    M=M+1 
+
+    @addr
+    D=M
+    @24576
+    D=A-D
+    @CLEAR_LOOP
+    D;JGT
+
+    @LOOP
+    0;JMP
+
+(draw)
+	// put bitmap location value in R12
+	// put code return address in R13
+	@SCREEN
+	D=A
+	@R12
+	AD=D+M
+	// row 8
+	@31806 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 9
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@31133 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 10
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@31807 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	AD=A+1 // D holds addr
+	M=1
+	// row 11
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@385 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	AD=A+1 // D holds addr
+	M=1
+	// row 12
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@32509 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	AD=A+1 // D holds addr
+	M=1
+	// row 13
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@32762 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 14
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@32756 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 15
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@16360 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 16
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@24624 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 17
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@12384 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 18
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@6592 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 19
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@1792 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// return
+	@LOOP
+	0;JMP
+
+//Actividad 4
+//Trataria de hacerlo de la misma manera que con la d pero teniendo en cuenta que e es @101 y que tendria el @clear. pdt: no funcionó y no tengo idea de por que, este solo dejó de funcionar (ya no dibuja)
+
+@24576
+D=M
+@100
+D=D-A       
+@draw
+D;JEQ       
+
+// Verifica si se presionó la tecla "e" (101)
+@24576
+D=M
+@101
+D=D-A
+@clear
+D;JEQ       
+
 @LOOP
 0;JMP
-
-(Clean LOOP)
-@0
-
-
 
 (draw)
 	// put bitmap location value in R12
