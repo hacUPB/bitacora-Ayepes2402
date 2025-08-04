5214ec88b65202b746370f9cc03b43cd735bcf60
Ayepes2402 <andrea.yepese@upb.edu.co>
2025-08-04 15:26:29 -0500

    tarea

---
+++ Experiencia de aprendizaje 2/actividades.asm
@@ -0,0 +1,304 @@
+//Actividad 3
+
+//Resultado bueno despues del intento
+(LOOP)
+@24576
+D=M
+@100
+D=D-A       
+@draw
+D;JEQ  
+@CLEAR
+@0
+D=M
+(CLEAR)
+    @SCREEN
+    D=A
+    @addr
+    M=D 
+
+(CLEAR_LOOP)
+    @addr
+    A=M
+    M=0  
+
+    @addr
+    M=M+1 
+
+    @addr
+    D=M
+    @24576
+    D=A-D
+    @CLEAR_LOOP
+    D;JGT
+
+    @LOOP
+    0;JMP
+
+(draw)
+	// put bitmap location value in R12
+	// put code return address in R13
+	@SCREEN
+	D=A
+	@R12
+	AD=D+M
+	// row 8
+	@31806 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 9
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@31133 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 10
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@31807 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	AD=A+1 // D holds addr
+	M=1
+	// row 11
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@385 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	AD=A+1 // D holds addr
+	M=1
+	// row 12
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@32509 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	AD=A+1 // D holds addr
+	M=1
+	// row 13
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@32762 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 14
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@32756 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 15
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@16360 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 16
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@24624 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 17
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@12384 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 18
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@6592 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 19
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@1792 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// return
+	@LOOP
+	0;JMP
+
+//Actividad 4
+(LOOP)
+@24576
+D=M
+@100
+D=D-A       
+@draw
+D;JEQ       
+
+// Verifica si se presionó la tecla "e" (101)
+@24576
+D=M
+@101
+D=D-A
+@clear
+D;JEQ   
+
+@LOOP
+0;JMP
+
+(draw)
+	// put bitmap location value in R12
+	// put code return address in R13
+	@SCREEN
+	D=A
+	@R12
+	AD=D+M
+	// row 8
+	@31806 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 9
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@31133 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 10
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@31807 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	AD=A+1 // D holds addr
+	M=1
+	// row 11
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@385 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	AD=A+1 // D holds addr
+	M=1
+	// row 12
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@32509 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	AD=A+1 // D holds addr
+	M=1
+	// row 13
+	D=A // D holds previous addr
+	@31
+	AD=D+A
+	@32762 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 14
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@32756 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 15
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@16360 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=A-D // RAM[addr]=-val
+	// row 16
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@24624 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 17
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@12384 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 18
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@6592 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// row 19
+	D=A // D holds previous addr
+	@32
+	AD=D+A
+	@1792 // A holds val
+	D=D+A // D = addr + val
+	A=D-A // A=addr + val - val = addr
+	M=D-A // RAM[addr] = val
+	// return
+	@LOOP
+	0;JMP
+	
+(clear)
+    @SCREEN
+    D=A
+    @addr
+    M=D 
+
+(CLEAR_LOOP)
+    @addr
+    A=M
+    M=0  
+
+    @addr
+    M=M+1 
+
+    @addr
+    D=M
+    @24576
+    D=A-D
+    @CLEAR_LOOP
+    D;JGT
+
+    @LOOP
+    0;JMP
+
+	(END)
+	@END
+	0;JMP
--- Experiencia de aprendizaje 2/reto.asm
+++ Experiencia de aprendizaje 2/reto.asm
@@ -1,312 +1,95 @@
-//Actividad 3
+//Escribe un programa en lenguaje ensamblador que sume los primeros 100 números naturales.
 
-//Resultado bueno despues del intento
+int i = 1;
+int sum = 0;
+While (i <= 100){
+   sum += i;
+   i++;
+}
+
+//enguaje ensamblador
+```
+@i
+M=1         
+@sum
+M=0         
+@limit
+M=100       
+
 (LOOP)
-@24576
-D=M
-@100
-D=D-A       
-@draw
-D;JEQ  
-@CLEAR
-@0
-D=M
-(CLEAR)
-    @SCREEN
-    D=A
-    @addr
-    M=D 
+@i
+D=M         
+@limit
+D=D-M       
+@END
+D;JGT       
 
-(CLEAR_LOOP)
-    @addr
-    A=M
-    M=0  
+@i
+D=M         
+@sum
+M=M+D       
 
-    @addr
-    M=M+1 
-
-    @addr
-    D=M
-    @24576
-    D=A-D
-    @CLEAR_LOOP
-    D;JGT
-
-    @LOOP
-    0;JMP
-
-(draw)
-	// put bitmap location value in R12
-	// put code return address in R13
-	@SCREEN
-	D=A
-	@R12
-	AD=D+M
-	// row 8
-	@31806 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 9
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@31133 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	// row 10
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@31807 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	AD=A+1 // D holds addr
-	M=1
-	// row 11
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@385 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	M=1
-	// row 12
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@32509 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	AD=A+1 // D holds addr
-	M=1
-	// row 13
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@32762 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	// row 14
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@32756 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	// row 15
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@16360 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	// row 16
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@24624 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 17
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@12384 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 18
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@6592 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 19
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@1792 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// return
-	@LOOP
-	0;JMP
-
-//Actividad 4
-(LOOP)
-@24576
-D=M
-@100
-D=D-A       
-@draw
-D;JEQ       
-
-// Verifica si se presionó la tecla "e" (101)
-@24576
-D=M
-@101
-D=D-A
-@clear
-D;JEQ   
+@i
+M=M+1       
 
 @LOOP
+0;JMP       
+
+(END)
+@END
+0;JMP       
+
+
+//Transforma el programa en alto nivel anterior para que utilice un ciclo for en vez de un ciclo while.
+
+int sum = 0;
+for (int i = 1; i <= 100; i++) {
+    sum += i;
+}
+
+//Escribe un programa en lenguaje ensamblador que implemente el programa anterior.
+
+@sum
+M=0 
+
+@i
+M=1
+
+@limit
+M=100 
+
+(FOR_LOOP)
+@i
+D=M
+@limit
+D=D-M
+@END
+D;JGT
+
+@i
+D=M
+@sum
+M=M+D
+
+@i
+M=M+1
+
+@FOR_LOOP
+0;JMP 
+
+(END)
+@END
 0;JMP
 
-(draw)
-	// put bitmap location value in R12
-	// put code return address in R13
-	@SCREEN
-	D=A
-	@R12
-	AD=D+M
-	// row 8
-	@31806 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 9
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@31133 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	// row 10
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@31807 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	AD=A+1 // D holds addr
-	M=1
-	// row 11
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@385 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	AD=A+1 // D holds addr
-	M=1
-	// row 12
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@32509 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	AD=A+1 // D holds addr
-	M=1
-	// row 13
-	D=A // D holds previous addr
-	@31
-	AD=D+A
-	@32762 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	// row 14
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@32756 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	// row 15
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@16360 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=A-D // RAM[addr]=-val
-	// row 16
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@24624 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 17
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@12384 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 18
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@6592 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// row 19
-	D=A // D holds previous addr
-	@32
-	AD=D+A
-	@1792 // A holds val
-	D=D+A // D = addr + val
-	A=D-A // A=addr + val - val = addr
-	M=D-A // RAM[addr] = val
-	// return
-	@LOOP
-	0;JMP
-	
-(clear)
-    @SCREEN
-    D=A
-    @addr
-    M=D 
+//Traduce este programa a lenguaje ensamblador:
+int a = 10;
+int *p;
+p = &a;
+*p = 20;
 
-(CLEAR_LOOP)
-    @addr
-    A=M
-    M=0  
-
-    @addr
-    M=M+1 
-
-    @addr
-    D=M
-    @24576
-    D=A-D
-    @CLEAR_LOOP
-    D;JGT
-
-    @LOOP
-    0;JMP
-
-	(END)
-	@END
-	0;JMP
-
-//Actividad 5.4
-
+//Lenguaje ensamblador
 @10 
-A=D 
+D=A 
 @var
 M=D
 @var
